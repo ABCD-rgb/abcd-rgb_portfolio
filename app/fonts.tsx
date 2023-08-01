373435f2f51970c2448457887fc7eeee454a4b0a
ABCD-rgb <agdelmo1@up.edu.ph>
2023-08-01 15:46:59 +0800

    style: added necessary fonts

---
+++ app/fonts.tsx
@@ -0,0 +1,12 @@
+import { Outfit, Roboto } from 'next/font/google'
+
+export const outfit = Outfit({
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+export const roboto = Roboto({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['100', '400', '700']
+})
